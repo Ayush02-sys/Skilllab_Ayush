719ec141dde235060db49ccf44b956c78afe2909
Ayush02-sys <ayuyadav2020@gmail.com>
2026-08-05 11:57:36 +0530

    updates Add and Loop functions

--- c1/05_08_26/Add.py
+++ c1/05_08_26/Add.py
@@ -8,3 +8,8 @@ print(f"Sum of {a} and {b} is {a*b}")
 print(f"Sum of {a} and {b} is {a/b}")
 print(f"Sum of {a} and {b} is {a//b}")
 
+a=int(input("enter the first number = "))
+b=int(input("enter the second number = "))
+print(f"Sum of first and second number is = {a+b}")
+
+print(f"sum of 1st and 2nd no. is = {(int(input("enter 1st no. = "))+int(input("enter 2nd no. = ")))}")
---
+++ c1/05_08_26/Loop.py
@@ -0,0 +1,27 @@
+a=range(5)
+b=range(1,10)
+c=range(1,10,2)
+
+for x in a:
+    print(x, end=" ")
+print(" ")
+
+for x in b:
+    print(x, end=" ")
+print(" ")
+
+for x in c:
+    print(x, end=" ")
+print("\n")
+
+for i in range(1,16,3):
+    print(f"{i} = hello world")
+print("\n")
+
+for i in range(1,16,3):
+    print(f"{i}", end=" ")
+print("\n")
+
+x=int(input("enter no."))
+for i in range(1,11):
+    print(f"{i} * {x} = {i*x}")
