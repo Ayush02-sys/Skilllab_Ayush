dbecf3d16bb02786f8310c9cbba526c384a5cc38
Ayush02-sys <ayuyadav2020@gmail.com>
2026-08-24 12:26:43 +0530

    intro to pandas

---
+++ c2/24_08_2026/panda.py
@@ -0,0 +1,9 @@
+#Pandas Training
+# **how to install library**
+# in terminal do : pip install pandas
+# used for data cleaning and preprocessing
+
+import pandas as pd
+s=pd.series([105,"amit",50])
+a=[[30,40,50],['a21','a22','a23'],['a31','a32','a33']]
+
