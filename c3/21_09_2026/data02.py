747c8bb0fada59c5656b064126bdf8e183563086
Ayush02-sys <ayuyadav2020@gmail.com>
2026-09-21 11:47:05 +0530

    seaborn class

---
+++ c3/21_09_2026/data02.py
@@ -0,0 +1,29 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+#1st way
+
+# g=np.random.default_rng(42)
+# d=g.integers(25,51,size=(5,6))
+# dg=["user1","user2","user3","user4","user5"]
+# health_rec=["sugar","bp","BMI","spo2","risk score","heart_rate"]
+# print(plt.figure(figsize=(10,6)))
+# df=pd.DataFrame(data=d, index=dg, columns=health_rec)
+# print(df)
+# print("-"*50)
+# print()
+
+#or
+
+#2nd way
+
+np.random.seed(42)
+df=pd.DataFrame({
+    "Sleeping_Hours":np.random.randint(1,15,24),
+    "Walk_hours":np.random.randint(20,40,24),
+    "workout_hours":np.random.randint(5,30,24),
+    "Assesment":np.random.randint(2,15,24)
+})
+print(df)
